0f391951be8224091926430d48eca4eb429cdaec
Jayanth <jayanth.sms.in@gmail.com>
2024-01-02 11:50:18 +0530

    first commit adding backend & client folders

---
+++ backend/models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+  tags: { type: [String] },
+  date: { type: Date, default: Date.now },
+});
+
+PostSchema.pre('save', (next) => {
+  now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+module.exports = mongoose.model('Post', postSchema);
